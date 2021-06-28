4c3f020970f9dd6ec077c6ce956397201a73594e
arm-B <arm0258@naver.com>
2021-06-29 02:03:58 +0900

    5.0 Get My Own Nweets

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,13 +1,24 @@
-import { authService } from "myBase";
-import React from "react";
+import { authService, dbService } from "myBase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
