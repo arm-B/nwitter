0663095c5361b2218ea70ff5217d6acd761ca064
arm-B <arm0258@naver.com>
2021-06-28 22:23:55 +0900

    from 3.0 to 3.3

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,5 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "myBase";
 
 //export default () => <span>Home</span>;
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  /*const getNweets = async () => {
+    const dbnweets = await dbService.collection("nweets").get(); //all of them, Querysnapshot
+    dbnweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]); //common >>> prev...
+    }); //show only data
+    console.log(dbnweets);
+  };*/
+
+  useEffect(() => {
+    //getNweets();
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
